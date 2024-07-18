689ad81ab94800c9d4a7eb020e04dc5be8ed8d8e
wroldofhigh <videochannel417@gmail.com>
2024-07-18 12:30:19 +0800

    commit3

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
+use App\Http\Controllers\comment;
+use App\Http\Controllers\post;
+
 
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+
+
+    
 });
+
+Route::resource('comment',commentController::class);
+Route::resource('post',postController::class);
