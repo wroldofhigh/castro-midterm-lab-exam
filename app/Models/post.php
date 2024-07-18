f1050e9bb275497c1623254e6b11e0a54fc1300e
wroldofhigh <videochannel417@gmail.com>
2024-07-18 11:01:32 +0800

    laravel model

---
+++ app/Models/post.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+use App\Models\user;
+use App\Models\post_comment;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class post extends Model
+{
+    use HasFactory;
+}
---
+++ app/Models/post_comment.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+use App\Models\comment_replies;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class post_comment extends Model
+{
+    use HasFactory;
+}
